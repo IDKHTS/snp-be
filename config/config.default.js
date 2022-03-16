8f056a2abfb05bad5bc3460920d767e9d82d3bbf
IDKHTS <hggpersonal@163.com>
2022-03-16 14:04:59 +0800

    feat: connect database

--- config/config.default.js
+++ config/config.default.js
@@ -10,5 +10,15 @@ module.exports = (appInfo) => {
             level: 'DEBUG', // console all logs to logfile
             consoleLevel: 'DEBUG',  // console all logs to terminal
         },
+        sequelize: {
+            dialect: 'mysql',
+            // host: '62.234.68.217',
+            // port: 3306,
+            // database: 'test_db',
+            host: '45.79.65.203',
+            port: 3306,
+            password: '123456',
+            database: 'egg-sequelize-doc-default',
+        }
     }
 }
